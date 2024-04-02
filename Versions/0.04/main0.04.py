47826db7d4313f250fc677170f98952726b3849a
99Maza99 <70022071+99Maza99@users.noreply.github.com>
2024-04-03 00:14:40 +0200

    Add files via upload

---
+++ Versions/0.04/main0.04.py
@@ -0,0 +1,12 @@
+#Imported libraries (Some maybe downloaded)
+import tkinter
+
+
+#Self made libraries
+import EntryGUI
+
+
+if __name__ == "__main__":
+
+    ##Starts Threads for the GUIs
+    EntryGUI
